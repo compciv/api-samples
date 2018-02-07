63fa6a318a5aa69567736e0b9bbe6cf8196e1951
Dan Nguyen <dansonguyen@gmail.com>
2018-02-07 12:52:19 -0800

    hey

--- fetching/google_translate.py
+++ fetching/google_translate.py
@@ -1,6 +1,7 @@
 # https://cloud.google.com/translate/docs/reference/libraries#client-libraries-install-python
 from google.cloud import translate
 from os import environ
+import random
 environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'creds-google-cloud.json'
 
 
@@ -25,37 +26,38 @@ def helloworld():
 
 def get_translation(text, target_lang, source_lang='en'):
     client  = translate.Client()
-    translation = client.translate(text, target_language=target_lang,
-                                         source_language=source_lang)
-    return translation
+    result = client.translate(text,
+                                    target_language=target_lang,
+                                     source_language=source_lang)
+    # add metadata
+    result['source_language'] = source_lang
+    result['target_language'] = target_lang
+
+    return result
 
 
 def pick_random_languages(n=5, exclude_langs=['en']):
     client = translate.Client()
-    languages = [t for t in translate_client.get_languages() if t['language'] not in exclude_langs]
-
-    return random.sample(languages, n)
+    data = client.get_languages()
+    codes = [d['language'] for d in data if d['language'] not in exclude_langs]
+    return random.sample(codes, n)
 
 def telephone(start_text, n=5, source_lang='en'):
     results = []
     thetext = start_text
     orglang = thelang = source_lang
     randolangs = pick_random_languages(n)
+    randolangs.append('en')
 
-    for lang in randolangs:
-        nextlang = lang['language']
+    for langcode in randolangs:
+        nextlang = langcode
         result = get_translation(thetext,
                                 source_lang=thelang,
                                 target_lang=nextlang)
-
+        # add lang metadata
         thetext = result['translatedText']
         thelang = nextlang
         results.append(result)
-    ## one more translation back into the source language:
-    lastresult = get_translation(thetext,
-                                source_lang=thelang,
-                                target_lang=orglang)
 
-    results.append(lastresult)
 
     return results
